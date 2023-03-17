b8179a23f63e855e2da1d0ef846a6c439bfb59bb
amgd aly <amgdaly9@gmail.com>
2023-03-17 02:01:39 +0200

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(){
+/**
+ * main - entry point
+ * 
+ * description :a c program that prints with puts function
+ *
+ * return : alawys 0 (success)
+*/
+
+int main()
+{
 	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
 }
