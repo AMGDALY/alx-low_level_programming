cbb2c6e521ff7f4b7c52d6e7f0ae976716b0b966
amgd aly <amgdaly9@gmail.com>
2023-03-17 04:35:49 +0200

    sii

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: A c progrm using sizeof to print the sizeof various types.
+ * Description: A C progrm using sizeof to print the sizeof various types.
  *
  * Return: Alawys 0 (success)
  */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an int: %lu byte(s)\ni", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
