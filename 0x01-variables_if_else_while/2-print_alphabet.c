24bff9c0d25e1b1705d6e6f1a816c0c32c01891f
amgd aly <amgdaly9@gmail.com>
2023-03-18 02:15:25 +0200

    alphha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ *main - Entry pint
+ *Discription: 'Check for number if it is positive or negative'
+ *Return: always 0
+ */
+
+int main(void)
+{	
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	putchar('\n');
+	}
+	return (0);
+	
+}
