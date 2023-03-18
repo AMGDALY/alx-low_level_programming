c62c4ef37d8b5c2ff27ee134e813fb176ae1eb86
amgd aly <amgdaly9@gmail.com>
2023-03-18 03:03:16 +0200

    dyup

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry pint
+ *Discription: 'Check for number if it is positive or negative'
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	return (0);
+}
