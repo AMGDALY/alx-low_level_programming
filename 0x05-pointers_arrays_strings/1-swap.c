3e3fda7d5ecefccf9ee0340b17d1d8f93466c857
amgd aly <amgdaly9@gmail.com>
2023-03-28 04:03:29 +0200

    dsad

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
- * sawp_int : swap the values of two integers
+ * swap_int - swap the values of two integers
+ *             using two input parameters
  *
  * @a : input parameter 1
  * @b : input number 2
  *
  * return : nothing
  */
+
 void swap_int(int *a, int *b)
 {
 	int temp;
