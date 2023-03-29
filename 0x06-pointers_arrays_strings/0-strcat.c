4bf56f7fce162633daf38dd7b4609f2d1d808e88
amgd aly <amgdaly9@gmail.com>
2023-03-30 01:16:52 +0200

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,13 @@ char *_strcat(char *dest, char *src)
 	int c, c2;
 
 	c = 0;
+	/*find the size of dest arry*/
 	while (dest[c])
 		c++;
+
+	/*iterate through each src array value without the null nyte*/
 	for (c2 = 0; src[2]; c2++)
+		/*append src[c2] to dest [c] while overwritting the null byte in dest*/
 		dest[c++] = src[c2];
 
 	return (dest);
