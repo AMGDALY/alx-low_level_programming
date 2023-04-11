66f9e9deae6f01b16756f539a065c75cb6fdc776
amgd aly <amgdaly9@gmail.com>
2023-04-11 02:54:42 +0200

    static1

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str + 0);
+		str++;
+	}
+	putchar('\n');
+}
+
+
